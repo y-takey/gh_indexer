9454430b9f7915d2e3346e2120daf0f2bd19d923
takeyuichi <y.takey@gmail.com>
2017-02-19 16:40:32 +0900

    Add link to subtitles

--- js/content.js
+++ js/content.js
@@ -5,11 +5,16 @@ script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/j
 var head=document.getElementsByTagName('head')[0];
 head.appendChild(script);
 
-const INDENT = { h1: 0, h2: 1, h3: 2, h4: 3 };
-const SPACE = "&nbsp;&nbsp;&nbsp;"
+const INDENTS = { h1: 0, h2: 1, h3: 2, h4: 3 };
+const INDENT_SIZE = 12
 const contents = $("h1,h2,h3,h4", ".markdown-body").map((i, el)=>{
-  let item = SPACE.repeat(INDENT[el.tagName.toLowerCase()]) + $(el).text().trim()
-  return $("<a>", { class: "kukkoro-item", html: item });
+  let indent = INDENTS[el.tagName.toLowerCase()];
+  return $("<a>", {
+    class: "kukkoro-item",
+    html: el.innerText.trim(),
+    href: $("a", el).attr("href"),
+    style: `margin-left: ${INDENT_SIZE * indent}px;`
+  });
 })
 const sidebar = $("<nav></nav>", { class: "kukkoro-sidebar" })
 $("body").append(sidebar);
