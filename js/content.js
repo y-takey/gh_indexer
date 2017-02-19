ba26722c8a8797cbefe06da205004729229636ab
takeyuichi <y.takey@gmail.com>
2017-02-19 20:48:29 +0900

    refresh when the page is transitioned

--- js/content.js
+++ js/content.js
@@ -6,16 +6,33 @@ var head=document.getElementsByTagName('head')[0];
 head.appendChild(script);
 
 const INDENTS = { h1: 0, h2: 1, h3: 2, h4: 3 };
-const INDENT_SIZE = 12
-const contents = $("h1,h2,h3,h4", ".markdown-body").map((i, el)=>{
-  let indent = INDENTS[el.tagName.toLowerCase()];
-  return $("<a>", {
-    class: "kukkoro-item",
-    html: el.innerText.trim(),
-    href: $("a", el).attr("href"),
-    style: `margin-left: ${INDENT_SIZE * indent}px;`
+const INDENT_SIZE = 12;
+let sidebar = null;
+
+const renderSidebar = ()=> {
+  if (sidebar) sidebar.remove();
+  if ($(".markdown-body").length === 0) return;
+
+  const contents = $("h1,h2,h3,h4", ".markdown-body").map((i, el)=>{
+    let indent = INDENTS[el.tagName.toLowerCase()];
+    return $("<a>", {
+      class: "kukkoro-item",
+      html: el.innerText.trim(),
+      href: $("a", el).attr("href"),
+      style: `margin-left: ${INDENT_SIZE * indent}px;`
+    });
   });
-})
-const sidebar = $("<nav></nav>", { class: "kukkoro-sidebar" })
-$("body").append(sidebar);
-sidebar.append(contents.toArray())
+  sidebar = $("<nav></nav>", { class: "kukkoro-sidebar" });
+  $("body").append(sidebar);
+  sidebar.append(contents.toArray());
+}
+
+renderSidebar();
+
+let renderedPath = location.pathname;
+$(window).on("transitionend", ()=> {
+  if (renderedPath === location.pathname) return;
+
+  setTimeout(renderSidebar, 1000)
+  renderedPath = location.pathname;
+});
