b45afe22d66233fd87db26b02968c5c74f7271a8
takeyuichi <y.takey@gmail.com>
2017-02-19 21:28:27 +0900

    comment out debugging code

--- js/content.js
+++ js/content.js
@@ -1,9 +1,11 @@
-// Inject jquery lib to document for debug
-var script=document.createElement('script');
-script.setAttribute("type", "text/javascript")
-script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")
-var head=document.getElementsByTagName('head')[0];
-head.appendChild(script);
+//////////////////////////////////////////////
+//// Inject jquery lib to document for debug
+//////////////////////////////////////////////
+// var script=document.createElement('script');
+// script.setAttribute("type", "text/javascript")
+// script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")
+// var head=document.getElementsByTagName('head')[0];
+// head.appendChild(script);
 
 const INDENTS = { h1: 0, h2: 1, h3: 2, h4: 3 };
 const INDENT_SIZE = 12;
