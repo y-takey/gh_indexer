5273ac6fa62fba4d8b41eb25c88a5545a847ca06
takeyuichi <y.takey@gmail.com>
2017-09-23 11:46:57 +0900

    Disable inject-script

--- dev/inject-script.js
+++ dev/inject-script.js
@@ -1,16 +1,16 @@
 //////////////////////////////////////////////
 //// Inject jquery lib to document for debug
+//   FIXME: This injection don't works fine.
 //////////////////////////////////////////////
-chrome.management.getSelf (self => {
-  // if (self.installType !== 'development') return;
 
-  chrome.tabs.executeScript({ code: 'var script=document.createElement("script");' +
-    'script.setAttribute("type", "text/javascript");' +
-    'script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js");' +
-    'document.getElementsByTagName("head")[0].appendChild(script);'
-  });
-  // let script=document.createElement('script');
-  // script.setAttribute("type", "text/javascript")
-  // script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js")
-  // document.getElementsByTagName('head')[0].appendChild(script);
-})
+// chrome.management.getSelf(self => {
+//   // if (self.installType !== 'development') return;
+//
+//   chrome.tabs.executeScript({
+//     code:
+//       'var script=document.createElement("script");' +
+//       'script.setAttribute("type", "text/javascript");' +
+//       'script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js");' +
+//       'document.getElementsByTagName("head")[0].appendChild(script);'
+//   });
+// });
